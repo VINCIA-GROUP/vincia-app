9cc395105e4f3cecce99c870acc527beb53e3200
Igor de Brito Coura <igor-coura@hotmail.com>
2023-04-26 21:09:14 -0300

    Create home page

--- client/vincia/lib/modules/home/presenter/page/home_page.dart
+++ client/vincia/lib/modules/home/presenter/page/home_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter_modular/flutter_modular.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  static const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,38 +32,164 @@ class HomePage extends StatelessWidget {
           ),
           actions: [
             Padding(
-              padding: EdgeInsets.all(8.0),
-              child: CircleAvatar(
-                child: Image(
-                    image: AssetImage("assets/images/avatar_default.png")),
+              padding: const EdgeInsets.all(4.0),
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1),
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                ),
+                child: Row(
+                  children: [
+                    const Text(
+                      "1255",
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.arrow_drop_up,
+                          color: Colors.green,
+                        ),
+                        Text(
+                          "20",
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 10),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            )
+            ),
+            PopupMenuButton(
+              icon: Row(
+                children: const [
+                  CircleAvatar(
+                    child: Image(
+                        image: AssetImage("assets/images/avatar_default.png")),
+                  ),
+                  Icon(Icons.arrow_drop_down_sharp)
+                ],
+              ),
+              itemBuilder: (BuildContext context) {
+                return const <PopupMenuEntry>[
+                  PopupMenuItem(
+                    value: 1,
+                    child: Text('Opção 1'),
+                  ),
+                  PopupMenuItem(
+                    value: 2,
+                    child: Text('Opção 2'),
+                  ),
+                  PopupMenuItem(
+                    value: 3,
+                    child: Text('Log out'),
+                  ),
+                ];
+              },
+              onSelected: (value) {
+                // Aqui você pode fazer algo quando uma opção for selecionada
+                print('Opção selecionada: $value');
+              },
+            ),
           ],
         ),
         body: Column(
           children: [
-            FloatingActionButton(
-              heroTag: "b",
-              onPressed: () async {
-                var auth = Modular.get<Auth0>();
-                const scheme = String.fromEnvironment("AUTH0_CUSTOM_SCHEME");
-                await auth.webAuthentication(scheme: scheme).logout();
-                Modular.to.navigate("/");
-              },
-              child: const Icon(Icons.access_time_sharp),
-            ),
+            Expanded(child: Container()),
             Padding(
-              padding: const EdgeInsets.all(8),
-              child: FloatingActionButton(
-                heroTag: "a",
-                onPressed: () async {
-                  //var auth = Modular.get<Auth0>();
-                  Modular.to.navigate("/");
-                },
-                child: const Icon(Icons.abc_outlined),
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Container(
+                constraints: const BoxConstraints(
+                  maxHeight: 110,
+                ),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    _carouselButton(
+                      "Questões",
+                      Icons.checklist_rtl,
+                      () {},
+                    ),
+                    _carouselButton(
+                      "Simulado",
+                      Icons.ballot_sharp,
+                      () {},
+                    ),
+                    _carouselButton(
+                      "Redação",
+                      Icons.create,
+                      () {},
+                    ),
+                    _carouselButton(
+                      "Estatisticas",
+                      Icons.stacked_bar_chart_outlined,
+                      () {},
+                    ),
+                    _carouselButton(
+                      "Test log out",
+                      Icons.logout,
+                      () async {
+                        var auth = Modular.get<Auth0>();
+                        const scheme =
+                            String.fromEnvironment("AUTH0_CUSTOM_SCHEME");
+                        await auth.webAuthentication(scheme: scheme).logout();
+                        Modular.to.navigate("/");
+                      },
+                    ),
+                    _carouselButton(
+                      "Test return init",
+                      Icons.login,
+                      () async {
+                        Modular.to.navigate("/");
+                      },
+                    ),
+                  ],
+                ),
               ),
             )
           ],
         ));
+  }
+
+  Widget _carouselButton(String text, IconData icon, Function() onPressed) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return SizedBox(
+            width: constraints.maxHeight,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+                foregroundColor:
+                    Theme.of(context).colorScheme.onPrimaryContainer,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+              ),
+              onPressed: onPressed,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(icon),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  FittedBox(
+                    child: Text(
+                      text,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
