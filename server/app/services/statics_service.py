f64d9446ba8bb550b5acf97ca623b5d03674e35f
LUCAS RIBEIRO GONCALVES <19.00194-0@maua.br>
2023-08-23 08:47:07 -0300

    criando servico para estatiscas

---
+++ server/app/services/statics_service.py
@@ -0,0 +1,24 @@
+
+
+class StaticsService:
+
+   def __init__(self, question_repository):
+      self.question_repository = question_repository
+
+   #MOCK EXAM
+   #(general, area, difficult) hit rate (last 30 days)
+   def get_hit_rate_mock():
+
+   #(general, area) grade and evolution (last 30 days)
+   def get_grade_mock():
+
+   #(general, area, difficult) time by question (last 30 days)
+   def get_time_by_question_mock():
+
+   #ADAPTATIVE QUESTIONS
+   #(general, area, difficult) hit rate (last 30 days)
+   def get_hit_rate_adaptative():
+
+   #(general, area, difficult) time by question (last 30 days)
+   def get_time_by_question_mock():
+   
