25bea6a6ac6efa26e204ac477af680636019e0f9
Igor de Brito Coura <igor-coura@hotmail.com>
2023-09-16 20:18:45 -0300

    fix bugs

--- server/app/controllers/ability_controller.py
+++ server/app/controllers/ability_controller.py
@@ -4,6 +4,7 @@ from app import connection_pool
 from app.controllers.base_controller import success_api_response
 from app.decorator.requires_auth import requires_auth
 from app.repositories.abilities_rating_repository import AbilitiesRatingRepository
+from app.repositories.ability_repository import AbilityRepository
 from app.services.ability_service import AbilityService
 
 
@@ -13,7 +14,8 @@ from app.services.ability_service import AbilityService
 def get_question(): 
         connection = connection_pool.get_connection()
         ability_rating_repository = AbilitiesRatingRepository(connection)
-        service = AbilityService(ability_rating_repository)
+        abilities_repository = AbilityRepository(connection)
+        service = AbilityService(ability_rating_repository, abilities_repository)
         
         user_id = session.get('current_user').get('sub')
         response = service.get_average_rating(user_id)
