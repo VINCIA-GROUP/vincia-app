eeb37279eee96229525e3a3b5ce2a87774a0ce2a
Igor de Brito Coura <igor-coura@hotmail.com>
2023-10-18 22:50:46 -0300

    fix bugs

--- server/app/decorator/error_handler.py
+++ server/app/decorator/error_handler.py
@@ -6,4 +6,9 @@ from flask import jsonify, request
 def handle_auth_error(error):
     response = jsonify(sucess=False, errors=error.to_json())
     response.status_code = 400
+    print(error.to_json())
     return response
+
+@app.errorhandler(Exception)
+def handle_error(error):
+    print(error)
